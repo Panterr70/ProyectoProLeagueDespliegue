3515078361955ae8837349050acae2531444c4aa
avillanurr10 <avillanurr@educacion.navarra.es>
2026-05-04 21:56:30 +0200

    feat.Despliegue de Back Render

--- ProLeaguePrincipal/frontend/js/config/config.js
+++ ProLeaguePrincipal/frontend/js/config/config.js
@@ -1,9 +1,9 @@
 const isProduction = window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1";
 
 export const API_BASE_URL = isProduction 
-    ? "https://tu-backend-proleague.onrender.com" // Esto lo cambiaremos luego
+    ? "https://proleague-backend.onrender.com" 
     : "http://localhost:3000";
 
 export const SOCKET_URL = isProduction
-    ? "https://tu-backend-proleague.onrender.com"
+    ? "https://proleague-backend.onrender.com"
     : "http://localhost:3000";
